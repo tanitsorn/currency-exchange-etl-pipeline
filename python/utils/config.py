4b46e6e97b468bbda39280eb36494fa1f96250d3
Tanitsorn Sathon <297323227+tanitsorn@users.noreply.github.com>
2026-08-24 12:10:54 +0700

    Add unit tests and CI workflow

--- python/utils/config.py
+++ python/utils/config.py
@@ -1,10 +1,10 @@
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = int(os.getenv("DB_PORT"))
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = int(os.getenv("DB_PORT", "3306"))
+DB_NAME = os.getenv("DB_NAME", "currency_exchange")
